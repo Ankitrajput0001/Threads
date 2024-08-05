85b6bdfbec45ee2b2c6e81418e9a2d01461b8dc3
Ankitrajput0001 <ankitrajput44408@gmail.com>
2024-08-05 19:43:55 +0530

    Initial Commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Ankit Rajput on 05/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
